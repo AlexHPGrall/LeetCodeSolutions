f794c87876e181a58f5f8f66fadedfec41be407e
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2021-09-08 15:23:20 +0000

    Sync LeetCode submission - Shifting Letters (cpp)

---
+++ problems/shifting_letters/solution.cpp
@@ -0,0 +1,26 @@
+/*class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+        int n=s.length();
+       
+        int shit =0;
+        for(int i=n-1; i>=0;--i)
+        {
+            shit= (shit + shifts[i])%26;
+            s[i] = ((s[i]+shit) -'a')%26 + 'a';
+        }
+        return s;
+        }
+};*/
+class Solution {
+public:
+    string shiftingLetters(string s, vector<int>& shifts) {
+           int sum=0;
+for(int i=s.size()-1;i>=0;i--)
+{
+    sum=(sum+shifts[i])%26;
+    s[i]=(s[i]-'a'+sum)%26 +'a';
+}
+return s;
+}
+};

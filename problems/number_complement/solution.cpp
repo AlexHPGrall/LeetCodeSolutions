f2c0c7e969c632d1cbcb088852cbf834b8f57e65
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2021-12-27 00:41:15 +0000

    Sync LeetCode submission - Number Complement (cpp)

---
+++ problems/number_complement/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int tmp=num;
+        int mask=0;
+        while(tmp>>=1)
+        {
+            mask=(mask<<1) +1;   
+        }
+        return ~num &mask;
+    }
+};

e8439884bcd007517f5e94076caac011fbf923cf
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-01-11 14:00:07 +0000

    Sync LeetCode submission - Sum of Root To Leaf Binary Numbers (cpp)

---
+++ problems/sum_of_root_to_leaf_binary_numbers/solution.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int res=0;
+    int sumRootToLeaf(TreeNode* root, int sum =0) {
+        if(!root)
+        {   
+            return res;
+        }
+            
+        sum<<=1;
+        sum+=root->val;
+        if(!root->left && !root->right)
+        {
+            res += sum;
+            sum>>=1;
+            return res;
+        }
+        
+        sumRootToLeaf(root->left,sum);
+        sumRootToLeaf(root->right,sum);
+        sum>>=1;
+        return res;
+        
+    }
+};

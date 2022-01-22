9f11fb13a48513620ccf1ec17e70245e9d14c561
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-01-22 10:31:11 +0000

    Sync LeetCode submission - Stone Game (cpp)

--- problems/stone_game/solution.cpp
+++ problems/stone_game/solution.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        sort(piles.begin(), piles.end());
-        int n=piles.size();
-        int Alex=0, Lee =0;
-        for(int i=n-1; ~i; --i)
-        {
-            if(i%2 == 1)
-                Alex+=piles[i];
-            else
-                Lee +=piles[i];
-        }
-        
-        return (Alex > Lee);
+      return true;  
     }
 };

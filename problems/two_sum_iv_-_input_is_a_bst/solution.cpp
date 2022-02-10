08ee95b13c3c0fbfffadcb1c4f43b686e9224d4d
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-02-10 10:45:06 +0000

    Sync LeetCode submission - Two Sum IV - Input is a BST (cpp)

--- problems/two_sum_iv_-_input_is_a_bst/solution.cpp
+++ problems/two_sum_iv_-_input_is_a_bst/solution.cpp
@@ -9,39 +9,18 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-class BSTIterator {
-private:
-    stack<TreeNode*> st;
-    bool reverse = false;
-    void push(TreeNode* root) {
-        while (root != nullptr) {
-            st.push(root);
-            root = !reverse ? root->left : root->right;
-        }
-    }
-public:
-    BSTIterator(TreeNode* root, bool reverse = false) : reverse(reverse) {
-        push(root);
-    }
-    int next() {
-        TreeNode* top = st.top(); st.pop();
-        push(!reverse ? top->right : top->left);
-        return top->val;
-    }
-};
-
 class Solution {
 public:
+    unordered_map<int, int> mp;
     bool findTarget(TreeNode* root, int k) {
-        BSTIterator leftItr(root), rightItr(root, true);
-        int left = leftItr.next(), right = rightItr.next();
-        while (left < right) {
-            if (left + right == k) return true;
-            if (left + right < k)
-                left = leftItr.next();
-            else
-                right = rightItr.next();
+        if(!root)
+            return false;
+        if(mp[k-root->val])
+            return true;
+        else
+        {
+           mp[root->val]++;
+            return findTarget(root->left,k) || findTarget(root->right, k);
         }
-        return false;
     }
 };

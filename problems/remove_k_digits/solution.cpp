eeb4c81fd5bffc1b126a0919a3a6b2f39206a48e
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-02-18 09:05:15 +0000

    Sync LeetCode submission - Remove K Digits (cpp)

--- problems/remove_k_digits/solution.cpp
+++ problems/remove_k_digits/solution.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
     string removeKdigits(string num, int k) {
-        vector<queue<int>> vq(10);
-        int n=num.length();
+        vector<char> st;
+        int n=num.size();
+        if(k==n)
+            return "0";
         for(int i=0;i<n;++i)
-        {
-            vq[num[i]-'0'].push(i);
-        }
-                 
-        string res;
-
-        for(int i=0;i<n;++i)
-        {
-            bool f=false;
-            //cout<<i<<endl;
-            if(k==n-i)
-                break;
-            for(int j=0; k&&j<num[i]-'0';++j)
+        {       
+            //cout<<i<<" "<<st.size()<<endl;
+            if(st.size() + (n-i) == n-k)
             {
-                if(!vq[j].empty())
-                    //cout<<j<<" "<<vq[j].front()<<endl;
-                if(!vq[j].empty() && vq[j].front()-i <=k)
+                while(i<n)
                 {
-                    k--;
-                    f=true;
-                    break;
+                    st.push_back(num[i]);
+                    ++i;
                 }
+                break;
             }
-            if(!f)
-                res.push_back(num[i]);
-            //cout<<endl;
-            vq[num[i]-'0'].pop();
+            
+            while(!st.empty() && st.back()>num[i] && st.size()+n-i !=n-k)
+            {
+                st.pop_back();
+            }
+            st.push_back(num[i]);
         }
+         while(!st.empty() && st.size()>n-k)
+            {
+                st.pop_back();
+            }
+        string res;
+        int i=0;
+        
+        while(i<st.size() && st[i]=='0')
+            ++i;
+        while(i<st.size())
+        {
+            res.push_back(st[i]);
+            ++i;
+        }
+         
         if(res.empty())
             res="0";
-        int i=0;
-        while(i<res.size() && res[i]=='0')
-            ++i;
-        if(i>=res.size())
-            i=res.size()-1;
-        return res.substr(i);
+        return res;
     }
 };

1b39a23a0aa7654d9989d9b6e2d51675f2be2654
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-03-03 07:25:16 +0000

    Sync LeetCode submission - Add Strings (cpp)

--- problems/add_strings/solution.cpp
+++ problems/add_strings/solution.cpp
@@ -1,40 +1,57 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int m=num1.length(), n=num2.length();
-        int k=max(m,n);
-        bool carry=false;
-        vector<char> sum;
-        for(int i=m-1, j=n-1, l=0;l<k;--i, --j,++l)
+        int i=num1.length()-1;
+        int j=num2.length()-1;
+        string res;
+        int rem=0;
+        while(~i || ~j)
         {
-            int c1=0, c2=0;
-            if(i>=0)
-                c1=num1[i]-'0';
-            if(j>=0)
-                c2=num2[j]-'0';
-            c1+=c2;
-            if(carry)
+            //cout<<i<<" "<<j<<endl;
+            if(i<0)
             {
-                ++c1;
-                carry=false;
+                char c= num2[j]+rem;
+                rem=0;
+                if(c>'9')
+                {
+                    c-=10;
+                    rem=1;
+                }
+                res.push_back(c);
+                j--;
             }
                 
-            
-            if(c1>=10)   
+            else if(j<0)
             {
-                c1-=10;
-                carry=true;
+                char c= num1[i]+rem;
+                rem=0;
+                if(c>'9')
+                {
+                    c-=10;
+                    rem=1;
+                }
+                res.push_back(c);
+                i--;
             }
-            sum.push_back(c1+'0');
-            
+            else
+            {
+                char c= num1[i]+(num2[j]-'0')+rem;
+                rem=0;
+                if(c>'9')
+                {
+                    c-=10;
+                    rem=1;
+                }
+                res.push_back(c);
+                j--;i--;
+            }   
         }
-        if(carry)
-            sum.push_back('1');
-        k=sum.size();
-        string res;
-        for(int i=k-1; ~i;--i)
-            res.push_back(sum[i]);
-        
+        if(rem)
+            res.push_back('1');
+        for(int i=0;i<res.length()/2;++i)
+        {
+            swap(res[i],res[res.length()-i-1]);
+        }
         return res;
     }
 };

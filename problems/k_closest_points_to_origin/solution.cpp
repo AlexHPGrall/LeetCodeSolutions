d2ae51d3dd41216225b4f937f6b4b5d32a04e761
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-03-12 00:00:18 +0000

    Sync LeetCode submission - K Closest Points to Origin (cpp)

--- problems/k_closest_points_to_origin/solution.cpp
+++ problems/k_closest_points_to_origin/solution.cpp
@@ -1,56 +1,45 @@
 class Solution {
 public:
+    int n;
+   inline int SquareD(vector<int> &a)
+    {
+        return (a[0]*a[0])+(a[1]*a[1]);
+    }
+    void partition(int l, int r, vector<vector<int>> &p, int k)
+    {
+        int i=l-1;
+        int pi=l+(r-l)/2;
+        int pivot = SquareD(p[pi]);
+        swap(p[pi],p[r]);
+        for(int j=l;j<r;++j)
+        {
+            if(SquareD(p[j])<pivot)
+            {
+                
+                ++i;
+                swap(p[i],p[j]);
+            }
+        }
+        ++i;
+        swap(p[i],p[r]);    
+        if(i==k)
+            return;
+        else if(i<k)
+            partition(i+1,r,p,k);
+        else
+            partition(l,i-1,p,k);
+    }
     vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        return quickSelect(points, k);
-    }
-    
-private:
-    vector<vector<int>> quickSelect(vector<vector<int>>& points, int k) {
-        int left = 0, right = points.size() - 1;
-        int pivotIndex = points.size();
-        while (pivotIndex != k) {
-            // Repeatedly partition the vector
-            // while narrowing in on the kth element
-            pivotIndex = partition(points, left, right);
-            if (pivotIndex < k) {
-                left = pivotIndex;
-            } else {
-                right = pivotIndex - 1;
-            }
-        }
-        
-        // Return the first k elements of the partially sorted vector
-        return vector<vector<int>>(points.begin(), points.begin() + k);
-    };
+        int n=points.size();
+        if(k==n)
+            return points;
+        partition(0,n-1,points,k);
 
-    int partition(vector<vector<int>>& points, int left, int right) {
-        vector<int>& pivot = choosePivot(points, left, right);
-        int pivotDist = squaredDistance(pivot);
-        while (left < right) {
-            // Iterate through the range and swap elements to make sure
-            // that all points closer than the pivot are to the left
-            if (squaredDistance(points[left]) >= pivotDist) {
-                points[left].swap(points[right]);
-                right--;
-            } else {
-                left++;
-            }
+        vector<vector<int>> res(k,vector<int>(2));
+        for(int i=0;i<k;++i)
+        {
+            res[i]=points[i];
         }
-        
-        // Ensure the left pointer is just past the end of
-        // the left range then return it as the new pivotIndex
-        if (squaredDistance(points[left]) < pivotDist)
-            left++;
-        return left;
-    };
-
-    vector<int>& choosePivot(vector<vector<int>>& points, int left, int right) {
-        // Choose a pivot element of the vector
-        return points[left + (right - left) / 2];
-    }
-    
-    int squaredDistance(vector<int>& point) {
-        // Calculate and return the squared Euclidean distance
-        return point[0] * point[0] + point[1] * point[1];
+        return res;
     }
 };

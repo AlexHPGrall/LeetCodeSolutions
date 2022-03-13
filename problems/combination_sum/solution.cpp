bb5e6263610b006ce83724be1000fea6c1b36ac1
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-03-13 18:10:56 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- problems/combination_sum/solution.cpp
+++ problems/combination_sum/solution.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    void recFind(vector<int> &arr, vector<int> &curr,vector<vector<int>> &res, int index, int k)
+    vector<vector<int>> res;
+    void dfs(int start,vector<int> &can, vector<int> &s,int sum, int t)
     {
-        for(int i=index;i<arr.size();++i)
+        for(int i=start;i<can.size()&&sum+can[i]<=t;++i)
         {
-            if(arr[i] >k)
-                break;
-            curr.push_back(arr[i]);
-            if(arr[i]==k)
-                res.push_back(curr);
+            s.push_back(can[i]);
+            if(sum+can[i]==t)
+                res.push_back(s);
             else
-                recFind(arr, curr, res, i, k-arr[i]);
-            curr.pop_back();
-            
+                dfs(i,can,s,sum+can[i],t);
+            s.pop_back();
         }
     }
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        sort(candidates.begin(), candidates.end());
-        vector<vector<int>> res;
-        vector<int> cur;
-        recFind(candidates, cur, res,0,target);
+    vector<vector<int>> combinationSum(vector<int>& can, int target) {
+        sort(can.begin(),can.end());
+        vector<int> tmp;
+        dfs(0,can,tmp,0,target);
         return res;
     }
 };

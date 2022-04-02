4c013d983df6356569cab0196b757624787ced5c
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-04-02 18:20:18 +0000

    Sync LeetCode submission - Greatest Common Divisor of Strings (cpp)

--- problems/greatest_common_divisor_of_strings/solution.cpp
+++ problems/greatest_common_divisor_of_strings/solution.cpp
@@ -1,53 +1,21 @@
 class Solution {
 public:
     string gcdOfStrings(string str1, string str2) {
-        if(str1.length()<str2.length())
-            swap(str1, str2);
-        int xlen=0;
-        int mask=0, mask2=0;
-        for(int i=0;i<str1.length();++i)
+        int maxL=gcd(str1.size(), str2.size());
+        string s;
+        for(int i=0;i<maxL;++i)
         {
-            if(str1[i]!=str2[i%str2.length()])
+            if(str1[i] != str2[i])
                 return "";
-            mask|=(1<<(str1[i]-'A'));
+            else
+                s.push_back(str1[i]);
         }
-        mask2=mask;
-        for(int i=0;i<str2.length();++i)
-        {
-            mask2^=(1<<(str2[i]-'A'));
-            if(mask2==mask)
-            {
-                for(int j=0; j<str2.length(); ++j)
-                {
-                    if(str2[j]!= str2[j%(i+1)])
-                        goto end;
-                }
-                xlen=i;
-                break;
-                
-            } 
-end:    continue;
-        }
-        //cout<<xlen<<endl;
-        if(xlen)
-        {
-            xlen=(xlen/2);
-        }
-        else
-        {
-            xlen=str2.length()-1;
-        }
-        xlen++;
-        int m=str1.length()/(xlen);
-        int n = str2.length()/(xlen);
-        
-        xlen*= gcd(m,n);
-        string res;
-        for(int i=0;i<xlen;++i)
-        {
-            res.push_back(str2[i]);
-        }
-        return res;
-        
+        for(int i=0; i<str1.length();++i)
+            if(str1[i]!=s[i%maxL])
+                return "";
+        for(int i=0; i<str2.length();++i)
+            if(str2[i]!=s[i%maxL])
+                return "";
+        return s;
     }
 };

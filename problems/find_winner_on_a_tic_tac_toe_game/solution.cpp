e4c49b42834c59a70e3f6dfd0e99336e2bebc752
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-04-26 21:30:25 +0000

    Sync LeetCode submission - Find Winner on a Tic Tac Toe Game (cpp)

--- problems/find_winner_on_a_tic_tac_toe_game/solution.cpp
+++ problems/find_winner_on_a_tic_tac_toe_game/solution.cpp
@@ -1,61 +1,32 @@
-
 class Solution {
 public:
     string tictactoe(vector<vector<int>>& moves) {
-      int board[9];
-        for(int i=0;i<8;++i)
-            board[i]=0;
-      for(int i =0; i<moves.size(); i++)
-      {
-          int row =moves[i][0], col =moves[i][1];
-         if(i%2 ==0)
-         {
-              
-              board[row*3 +col] =1;
-        }
-        else
+       vector<vector<int>> grid(3, vector<int>(3,0));
+        int curr=1;
+        for(auto &m:moves)
         {
-          board[row*3 +col] =-1;  
+            grid[m[0]][m[1]] = curr;
+            curr*=-1;
         }
-          
-      }
-        for(int i=0; i<3; ++i)
+        for(int i=0;i<3;++i)
         {
-            if( board[i*3]&& (board[i*3 +0] == board[i*3 +1] && board[i*3 +1] == board[i*3 +2]))
-            {
-                if(board[i*3] == 1)
-                    return "A";
-                else
-                    return "B";
-            }
+            int row=grid[i][0]+grid[i][1]+grid[i][2];
+            
+            int col=grid[0][i]+grid[1][i]+grid[2][i];
+            if(row==3||col ==3)
+                return "A";
+            if(col==-3 || row==-3)
+                return "B";
         }
-        for(int i=0; i<3; ++i)
-        {
-            if( board[i] && (board[0+i] == board[3 +i] && board[3 +i] == board[6 +i]))
-            {
-                if(board[i] == 1)
-                    return "A";
-                else
-                    return "B";
-            }
-        }
-        if( board[0] && (board[0] == board[4] && board[4] == board[8]))
-            {
-                if(board[0] == 1)
-                    return "A";
-                else
-                    return "B";
-            }
-        if( board[2] && (board[2] == board[4] && board[4] == board[6]))
-            {
-                if(board[2] == 1)
-                    return "A";
-                else
-                    return "B";
-            }
-        if(moves.size() == 9)
-               return "Draw";
-               
+        int diag1=grid[0][0]+grid[1][1]+grid[2][2];
+
+        int diag2=grid[2][0]+grid[1][1]+grid[0][2];
+        if(diag1==3 || diag2==3)
+            return "A";
+        if(diag1 == -3 || diag2 ==-3)
+            return "B";
+        if(moves.size()==9)
+        return "Draw";
         return "Pending";
     }
 };

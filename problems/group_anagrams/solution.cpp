9c92346ad2871785601d672c2264af5fe3348aac
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-05-04 13:23:13 +0000

    Sync LeetCode submission - Group Anagrams (cpp)

--- problems/group_anagrams/solution.cpp
+++ problems/group_anagrams/solution.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, int> hashmap;
-        vector<vector<string>> res;
-        
-        for (string key : strs) {
-            string aux = key;
-            sort(key.begin(),key.end());
-            if (hashmap.count(key) ==1) {
-                res[hashmap[key]].push_back(aux);
-            } else if (hashmap.count(key) == 0){
-                hashmap.insert({key, res.size()});
-                vector<string> temp;
-                res.push_back(temp);
-                res[hashmap[key]].push_back(aux);
+        unordered_map<string,vector<int>> map;
+        for(int m=0; m<strs.size(); ++m)
+        {
+            int freq[26]={0};
+            for(char c:strs[m])
+            {
+                freq[c-'a']++;
             }
-            
+            string s;
+            for(int i=0; i<26;++i)
+            {
+                for(int j=0; j<freq[i];++j)
+                    s.push_back('a'+i);
+            }
+            map[s].push_back(m);
+        }
+        vector<vector<string>> res;
+        for(auto &[key, val]:map)
+        {
+            vector<string> tres;
+            for(int i:val)
+                tres.push_back(strs[i]);
+            res.push_back(tres);
         }
         return res;
     }

9e439f3adb2ce2da292d53477c41dad099779617
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-05-17 15:21:15 +0000

    Sync LeetCode submission - Find Common Characters (cpp)

--- problems/find_common_characters/solution.cpp
+++ problems/find_common_characters/solution.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
     vector<string> commonChars(vector<string>& words) {
-        int totalFreq[26]={0};
-        for(char c:words[0])
-            totalFreq[c-'a']++;
-        for(int j=1;j<words.size();++j)
-        {
-            string s=words[j];
-            int freq[26]={0};
-            for(char c:s)
-                freq[c-'a']++;
-            for(int i=0;i<26;++i)
-            {
-                totalFreq[i]=min(freq[i],totalFreq[i]);
-            }
-        }
+        int cFreq[26]={};
         vector<string> res;
+        for(char c:words[0])
+        {
+            cFreq[c-'a']++;
+        }
+        
+        for(int i=1;i<words.size();++i)
+        {
+            int freq[26]={};
+            for(char c:words[i])
+                freq[c-'a']++;
+            for(int j=0; j<26;++j)
+                cFreq[j]=min(freq[j],cFreq[j]);
+        }
         for(int i=0; i<26;++i)
         {
             string s;
-            s.push_back(i+'a');
-            while(totalFreq[i])
-            {
+            s.push_back('a'+i);
+            for(int j=0; j<cFreq[i];++j)
                 res.push_back(s);
-                totalFreq[i]--;
-            }
         }
         return res;
     }

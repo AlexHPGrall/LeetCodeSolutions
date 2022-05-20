2817a83eba8684bfdfd4494f85077bd77984e2e8
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-05-20 09:50:22 +0000

    Sync LeetCode submission - Unique Paths II (cpp)

--- problems/unique_paths_ii/solution.cpp
+++ problems/unique_paths_ii/solution.cpp
@@ -1,34 +1,24 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size(), n= obstacleGrid[0].size();
-        if(m==1 && n ==1)
-            return !obstacleGrid[0][0]?1:0;
-        vector<vector<int>> dp(m, vector<int>(n, -1));
-        
-        dp[0][0] =!obstacleGrid[0][0]?1:0;
-        for(int i = 1; i<n; ++i)
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        if(grid[0][0])
+            return 0;
+        int m=grid.size();
+        int n=grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0][0]=1;
+        for(int i=0;i<m;++i)
         {
-            if(!obstacleGrid[0][i])
-                dp[0][i] = dp[0][i-1];
-            else
-                dp[0][i] = 0;
-        }
-        for(int j = 1; j<m; ++j)
-        {
-            if(!obstacleGrid[j][0])
-                dp[j][0] = dp[j-1][0];
-            else
-                dp[j][0] = 0;
-        }
-        for(int j =1; j<m; ++j)
-        {
-            for(int i =1; i<n; ++i)
+            for(int j=0;j<n;++j)
             {
-                if(!obstacleGrid[j][i])
-                dp[j][i] = dp[j-1][i] + dp[j][i-1];
-                else
-                dp[j][i] = 0;
+                
+                if(grid[i][j]||(i==0&&j==0))
+                    continue;
+                dp[i][j]=0;
+                if(i>0)
+                    dp[i][j]+=dp[i-1][j];
+                if(j>0)
+                    dp[i][j]+=dp[i][j-1];
             }
         }
         return dp[m-1][n-1];

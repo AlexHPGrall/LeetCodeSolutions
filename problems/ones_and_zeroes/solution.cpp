add461f5f9f3dedfca12d7770774fd564c1c7f0e
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-05-23 10:59:26 +0000

    Sync LeetCode submission - Ones and Zeroes (cpp)

--- problems/ones_and_zeroes/solution.cpp
+++ problems/ones_and_zeroes/solution.cpp
@@ -1,17 +1,35 @@
 class Solution {
-    vector<int> zeroes;
-    vector<int> ones;
 public:
-   int findMaxForm(vector<string>& S, int M, int N) {
-        int dp[101][101]{0};
-        for (string str : S) {
-            int zeros = 0, ones = 0;
-            for (char c : str)
-                c == '0' ? zeros++ : ones++;
-            for (int i = M; i >= zeros; i--)
-                for (int j = N; j >= ones; j--)
-                    dp[i][j] = max(dp[i][j], dp[i-zeros][j-ones] + 1);
+    int dp[101][101][600]={};
+    int Dfs(int z, int o, int m, int n, vector<pair<int, int>> &zno, int index)
+    {
+        if(z>m || o>n)
+            return INT_MIN;
+        if(index==zno.size())
+            return 0;
+        if(dp[z][o][index]!=0)
+            return dp[z][o][index];
+        dp[z][o][index]= max(1+Dfs(z+zno[index].first, o+zno[index].second, m,n, zno,index+1), Dfs(z,o,m,n,zno,index+1));
+        return dp[z][o][index];
+    }
+    
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<pair<int, int>> zno;
+        for(auto &s:strs)
+        {
+            int z=0;
+            int o=0;
+            for(auto c:s)
+            {
+                if(c=='0')
+                    z++;
+                else
+                    o++;
+                
+            }
+            zno.push_back({z,o});
         }
-        return dp[M][N];
+        
+        return Dfs(0,0,m,n,zno,0);
     }
 };

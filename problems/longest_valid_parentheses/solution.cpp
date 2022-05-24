cdfb6f373f2dffd82d80e9b079f4634963418024
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-05-24 14:21:01 +0000

    Sync LeetCode submission - Longest Valid Parentheses (cpp)

--- problems/longest_valid_parentheses/solution.cpp
+++ problems/longest_valid_parentheses/solution.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
     int longestValidParentheses(string s) {
-        vector<int> stack ={-1};
-        int res = 0;
-        for(int i = 0; i< s.size(); ++i)
+        vector<int> dp(s.size(), 0);
+        int res=0;
+        for(int i=1;i<s.size();++i)
         {
-            if(s[i] == '(')
-                stack.push_back(i);
-            else if(stack.size() == 1)
-                stack[0] =i;
-            else
+            if(s[i]=='(')
+                continue;
+            if(s[i-1]=='(')
             {
-                stack.pop_back();
-                res = max(res, i-stack.back()); 
-                
+                dp[i]=2;
+                if(i-2>=0)
+                    dp[i]+=dp[i-2];
             }
+            else if(i-dp[i-1]-1>=0 &&s[i-dp[i-1]-1]=='(')
+            {
+                dp[i]=dp[i-1]+2;
+                if(i-dp[i-1]-2>=0)
+                    dp[i]+=dp[i-dp[i-1]-2];
+            }
+            res=max(res,dp[i]);
         }
         return res;
     }

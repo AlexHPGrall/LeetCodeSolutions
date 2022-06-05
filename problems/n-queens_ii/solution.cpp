a8bd9a6454591a1aeb881caf5ab62ccff62c475d
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-06-05 16:09:12 +0000

    Sync LeetCode submission - N-Queens II (cpp)

--- problems/n-queens_ii/solution.cpp
+++ problems/n-queens_ii/solution.cpp
@@ -1,46 +1,22 @@
 class Solution {
 public:
-    void solve(int row ,int n, vector<string> &board, int &res)
+    int dfs(int n, int row, int c, int d1, int d2)
     {
-        if(row ==n)
+        if(row==n)
+            return 1;
+        int res=0;
+        for(int col=0; col<n;++col)
         {
-            ++res;
-            return;
+            if(((1<<col)&c) || (1<<(row-col+n)&d1) || (1<<(row+col)&d2))
+                continue;
+            res+=dfs( n, row+1, c|(1<<col), d1|(1<<(row-col+n)), d2|(1<<(row+col)));
         }
-        for(int i = 0; i<n; ++i)
-        {
-            if(check(row, i, n, board))
-            {
-                board[row][i]='Q';
-                solve(row+1, n, board, res);
-                board[row][i]='.';
-            }
-            
-            
-        }
-    }
-    bool check(int row, int col, int n, vector<string> &board)
-    {
-        for(int i= row-1; i>=0; --i)
-        {
-            if(board[i][col] == 'Q')
-                return false;
-            if(col+(row-i)<n && board[i][col+(row-i)] == 'Q')
-                return false;
-            if(col-(row-i)>=0 && board[i][col-(row-i)] == 'Q')
-                return false;
-        }
-        return true;
-    }
-   int totalNQueens(int n) {
-        int res =0;
-        string s(n, '.');
-        vector<string> board(n, s);
-        solve(0,n,board,res);
-        
-       
-        
         return res;
+    }
+    int totalNQueens(int n) {
+        
+        return dfs(n, 0, 0, 0, 0);
+
         
     }
 };

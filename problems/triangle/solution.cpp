f257d33b63c61cfd9ffb7561574f48b51b49d167
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-06-13 03:47:08 +0000

    Sync LeetCode submission - Triangle (cpp)

--- problems/triangle/solution.cpp
+++ problems/triangle/solution.cpp
@@ -1,11 +1,36 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
-        for(int n = triangle.size() -2; n>=0; --n)
-            for(int i=0; i<triangle[n].size(); ++i)
+        int n=triangle.size();
+        if(n==1)
+            return triangle[0][0];
+        vector<int> dp1(n), dp2(n);
+        dp1[0]=triangle[0][0];
+        int res=INT_MAX;
+        for(int i=1;i<n;++i)
+        {
+            for(int j=0;j<=i;++j)
             {
-                triangle[n][i] += min(triangle[n+1][i], triangle[n+1][i+1]);
+                if(j==i)
+                {
+                    dp2[j]=dp1[j-1];
+                }
+                else if(j==0)
+                {
+                    dp2[j]=dp1[j];
+                }
+                else
+                    dp2[j]=min(dp1[j], dp1[j-1]);
+                dp2[j]+=triangle[i][j];
+                if(i==n-1)
+                {
+                    res=min(res, dp2[j]);
+                }
             }
-        return triangle[0][0];
+            swap(dp1,dp2);
+        }
+
+        return res;
+        
     }
 };

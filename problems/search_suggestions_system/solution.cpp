354e17f336aaebefcdafa4edbc63e766b8b3c748
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-06-19 23:47:52 +0000

    Sync LeetCode submission - Search Suggestions System (cpp)

--- problems/search_suggestions_system/solution.cpp
+++ problems/search_suggestions_system/solution.cpp
@@ -1,24 +1,63 @@
+struct TrieNode
+{
+    bool end=false;
+    TrieNode *next[26]={};
+};
 class Solution {
+    void insert(TrieNode *root, string &s)
+    {
+        for(char c:s)
+        {
+            if(!root->next[c-'a'])
+                root->next[c-'a']=new TrieNode;
+            root=root->next[c-'a'];
+        }
+        root->end=true;
+    }
+    void searchFromPrefix(TrieNode *root, string &prefix, vector<string> &v)
+    {
+        if(v.size()==3)
+            return;
+        if(root->end==true)
+            v.push_back(prefix);
+        if(v.size()==3)
+            return;
+        for(int i=0; i<26;++i)
+        {
+            if(root->next[i])
+            {
+                prefix.push_back('a'+i);
+                searchFromPrefix(root->next[i], prefix, v);
+                prefix.pop_back();
+                 if(v.size()==3)
+                    return;  
+            }
+        }
+    }
+    TrieNode *findPrefix(TrieNode *root, int index, string &prefix)
+    {
+        if(!root || index==prefix.size())
+            return root;
+        return findPrefix(root->next[prefix[index]-'a'], index+1, prefix);
+        
+    }
 public:
     vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
-        sort(products.begin(), products.end());
-        vector<vector<string>> ans;
-        int left =0, right = products.size() -1;
-        for(int i = 0; i<searchWord.length(); ++i)
+        TrieNode root;
+        vector<vector<string>> res;
+        for(string &w:products)
         {
-            vector<string> res;
-            char c = searchWord[i];
-            while(left<= right &&(products[left].length() == i || products[left][i]<c)) left++;
-            while(left<= right &&(products[right].length() == i || products[right][i]>c)) right--;
-            
-            for(int j =0; j<3 && left+j<=right;++j)
-            {
-                res.push_back(products[left+j]);
-                
-            }
-            ans.push_back(res);
+            insert(&root, w);
         }
-        return ans;
-        
+        for(int i=0;i<searchWord.size();++i)
+        {
+            string s=searchWord.substr(0,i+1);
+            TrieNode *prefixNode=findPrefix(&root,0, s);
+            vector<string> v;
+            if(prefixNode)
+               searchFromPrefix(prefixNode, s,v);
+            res.push_back(v);
+        }
+        return res;
     }
 };

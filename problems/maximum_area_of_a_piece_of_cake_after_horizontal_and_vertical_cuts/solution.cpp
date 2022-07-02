5d91f086a90a9131222066ba65620f74bb8dd857
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-07-02 21:52:30 +0000

    Sync LeetCode submission - Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts (cpp)

--- problems/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/solution.cpp
+++ problems/maximum_area_of_a_piece_of_cake_after_horizontal_and_vertical_cuts/solution.cpp
@@ -1,58 +1,19 @@
-
 class Solution {
 public:
-    
-    int Partition(vector<int>& A, int p, int r)
-    {
-        int x= A[r];
-        int i = p-1;
-        for(int j = p ;j<r; ++j)
-        {
-            if(A[j]<=x)
-            {
-                ++i;
-                int temp = A[j];
-                A[j] = A[i];
-                A[i] =temp;
-            }
-                
-        }
-        A[r] = A[i+1];
-        A[i+1] =x;
-        return i+1;
-        
-    }
-    void QuickSort(vector<int>& A, int p, int r)
-    {
-        if(p<r)
-        {
-            int q = Partition(A, p,r);
-            QuickSort(A, p , q-1);
-            QuickSort(A, q+1, r);
-        }
-    }
+    int mod =(1e9)+7;
     int maxArea(int h, int w, vector<int>& hCuts, vector<int>& vCuts) {
-            ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-         //sort(hCuts.begin(), hCuts.end());
-          //sort(vCuts.begin(), vCuts.end());
-            QuickSort(hCuts,0, hCuts.size()-1);
-            QuickSort(vCuts, 0, vCuts.size()-1);
-            int hmax =max(hCuts[0], h-hCuts[hCuts.size()-1]), vmax =max(vCuts[0],w-vCuts[vCuts.size()-1]) , mod = (1e9 +7);
-            
-            for(int i =1; i<hCuts.size(); ++i)
-            {
-                    hmax = max(hmax, hCuts[i]-hCuts[i-1]);
-            }
-            for(int i = 1; i<vCuts.size(); ++i)
-            {
-                 if(i<vCuts.size())
-                    vmax = max(vmax, vCuts[i]-vCuts[i-1]);
-        
-            }
-             
-            return ((long)hmax * (long)vmax)% mod;
-                
+        int m=hCuts.size();
+        int n=vCuts.size();
+        sort(begin(hCuts), end(hCuts));
+        sort(begin(vCuts), end(vCuts));
+        int x= hCuts[0];
+        int y=vCuts[0];
+        for(int i=1;i<m;++i)
+            x=max(x, hCuts[i]-hCuts[i-1]);
+        for(int i=1;i<n;++i)
+            y=max(y, vCuts[i]-vCuts[i-1]);
+        x=max(x, h-hCuts[m-1]);
+        y=max(y, w-vCuts[n-1]);
+        return (int)((((long long)x%mod)*((long long)y%mod))%mod);
     }
 };

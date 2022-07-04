596801085392249a7664fdc240bf2933a4da7769
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-07-04 10:25:49 +0000

    Sync LeetCode submission - Candy (cpp)

--- problems/candy/solution.cpp
+++ problems/candy/solution.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        vector<int> Candies(ratings.size(), 1);
-       for(int i= 1; i<ratings.size(); ++i)
-       {
-           if((ratings[i] < ratings[i-1]) && Candies[i] >= Candies[i-1])
-           {
-               Candies[i-1] = Candies[i] +1;
-           }
-           else if((ratings[i] > ratings[i-1]) && Candies[i] <= Candies[i-1])
-               Candies[i] = Candies[i-1] +1;
-       }
-        for(int i= ratings.size() -2; i>=0; --i)
-       {
-           if((ratings[i] < ratings[i+1]) && Candies[i] >= Candies[i+1])
-           {
-               Candies[i+1] = Candies[i] +1;
-           }
-           else if((ratings[i] > ratings[i+1]) && Candies[i] <= Candies[i+1])
-               Candies[i] = Candies[i+1] +1;
-       }
-        int sum = 0;
-        for(int c:Candies)
-            sum+=c;
-       return sum; 
+        int n=ratings.size();
+        int i=0;
+        vector<int> mem(n,1);
+        for(i=1; i<n;++i)
+        {
+            if(ratings[i]>ratings[i-1])
+                mem[i]=mem[i-1]+1;
+        }
+    
+        for(i=n-2; i>=0;--i)
+        {
+            if(ratings[i]>ratings[i+1])
+            {
+                if(i>0 && ratings[i]>=ratings[i+1])
+                    mem[i]=max(mem[i], mem[i+1]+1);
+                else
+                    mem[i]=mem[i+1]+1;
+            }
+           
+        }
+        
+        int res=0;
+        for(int num:mem)
+            res+=num;
+        return res;
     }
 };

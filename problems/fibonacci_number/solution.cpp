e61ad14538567032f169c96dd023ae9a5e4a1d2b
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-07-06 02:27:29 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- problems/fibonacci_number/solution.cpp
+++ problems/fibonacci_number/solution.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     int fib(int n) {
-        int a=0,b=1;
-        int c;
-        if(n==0)
-            return a;
-        if(n==1)
-            return b;
-        for(int i=2; i<=n;++i)
+        int c=2, b=1, a=0;
+        while(n--)
         {
-            c=a+b;
+            c=b+a;
             a=b;
             b=c;
-        }
-        return c;
+        }  
+            return a;
     }
 };

1a4f92dc40932bb2327545026c6c4c825521806d
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-07-07 05:31:19 +0000

    Sync LeetCode submission - Interleaving String (cpp)

--- problems/interleaving_string/solution.cpp
+++ problems/interleaving_string/solution.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
+    
     bool isInterleave(string s1, string s2, string s3) {
-        if(s3.size() != s1.size() + s2.size())
+        int m=s1.size();
+        int n=s2.size();
+        if(s3.size()!=m+n)
             return false;
-       vector<vector<bool>> dp(s1.size()+1, vector<bool>(s2.size()+1, true));
-        
-        for(int i = 1; i<=s1.size(); ++i)
-            dp[i][0] = dp[i-1][0]&&(s1[i-1] == s3[i-1]);
-        for(int j= 1; j<=s2.size(); ++j)
-            dp[0][j] =  dp[0][j-1] &&(s2[j-1] == s3[j-1]);
-        for(int i = 1; i<=s1.size(); ++i)
-        {
-            for(int j= 1; j<=s2.size(); ++j)
+        //vector<vector<int>> dp(m+1, vector<int>(n+1, false));
+        vector<bool> dp(n+1,false);
+            for(int i=0;i<=m;++i)
             {
-                dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1]) ||  (dp[i][j-1] && s2[j-1] == s3[i+j-1]);
+                for(int j=0;j<=n;++j)
+                {
+                    int k=i+j-1;
+                    if(i==0 && j==0)
+                        dp[j]=true;
+                    else if(i>0 && s1[i-1] == s3[k] && dp[j])
+                        dp[j]=true;
+                    else if(j>0 && s2[j-1]==s3[k] && dp[j-1])
+                        dp[j]=true;
+                    else
+                        dp[j]=false;
+                }
+                
             }
+        return dp[n];
         }
-        return dp[s1.size()][s2.size()];
-    }
 };

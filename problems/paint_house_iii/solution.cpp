6471fc8d1dbf71e9cb9e43fddafe6e5e7754cc45
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-07-08 08:42:17 +0000

    Sync LeetCode submission - Paint House III (cpp)

--- problems/paint_house_iii/solution.cpp
+++ problems/paint_house_iii/solution.cpp
@@ -1,46 +1,52 @@
 class Solution {
 public:
     int mx=1e7;
-    int dp[101][21][101]={};
-    int Dfs(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target, int i, int nei)
-    {
-        if(nei>target)
-            return mx;
-        if(i==houses.size())
+
+    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
+        int dp[101][21][101]={};
+        for(int i=0;i<=m;++i)
         {
-            if(nei+1==target)
-              return 0;
-            else 
-                return mx;
-        }
-        if(houses[i]!=0)
-        {
-            if(i>0 && houses[i-1]!=houses[i])
-                return Dfs(houses, cost, m, n, target, i+1, nei+1);
-            else
+            for(int j=1;j<=n;++j)
             {
-                return Dfs(houses, cost, m, n, target, i+1, nei);
+                for(int k=0; k<=m;++k)
+                {
+                    
+                    if(k>i || (k==0 && i>0))
+                    {   
+                        dp[i][j][k]=mx;
+                        continue;
+                    }
+                    if(i==0)
+                        continue;
+                    dp[i][j][k]=mx;
+                    if(houses[i-1]!=0)
+                    {   
+                        if(houses[i-1]==j)
+                        {
+                            dp[i][j][k]=dp[i-1][j][k];
+                            for(int l=1;l<=n;++l)
+                            {
+                                if(l==j)
+                                    continue;
+                                dp[i][j][k]=min(dp[i][j][k], dp[i-1][l][k-1]);
+                            }
+                                
+                        }
+                        continue;
+                    }
+                    for(int l=1;l<=n;++l)
+                    {
+                        if(l!=j)
+                            dp[i][j][k]=min(dp[i][j][k], cost[i-1][j-1]+dp[i-1][l][k-1]);
+                        else
+                            dp[i][j][k]=min(dp[i][j][k], cost[i-1][j-1]+dp[i-1][l][k]);
+                    }
+                }
             }
         }
-        if(i>0 &&dp[i][houses[i-1]][nei]!=0)
-            return dp[i][houses[i-1]][nei];
         int res=mx;
         for(int j=1;j<=n;++j)
-        {
-            houses[i]=j;
-            if(i>0 && houses[i-1]!=houses[i])
-                res=min(res, cost[i][j-1]+Dfs(houses, cost, m,n,target, i+1, nei+1));
-            else
-                res=min(res, cost[i][j-1]+Dfs(houses, cost, m,n,target, i+1, nei));
-            houses[i]=0;
-        }
-        if(i>0)
-            dp[i][houses[i-1]][nei]=res;
-        return res;
-    }
-    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        int res=Dfs(houses, cost, m,n,target,0,0);
-        
+            res=min(res,dp[m][j][target]);
         return res>=mx?-1:res;
     }
 };

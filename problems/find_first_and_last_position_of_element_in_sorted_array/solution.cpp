11eb6ce6fe58b16454a7ecb14f2969ec2c01e80c
AlexHPGrall <68565542+AlexHPGrall@users.noreply.github.com>
2022-07-25 04:05:46 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (cpp)

--- problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
+++ problems/find_first_and_last_position_of_element_in_sorted_array/solution.cpp
@@ -1,58 +1,11 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-        int L=0;
-        int R = n;
-        int m;
-        int right =-1;
-        int left = -1;
-        int rres, lres;
-        vector<int> wrong =vector<int>({-1,-1});
-        if(n==0 || nums[0] > target || nums[n-1] < target)
-            return wrong;
-        if(n==1)
-            return vector<int>({0,0});
-        while(L<R)
-        {
-            m=floor((L+R)/2);
-            if(nums[m] < target)
-            {
-                L=m+1;
-            }
-            else
-            {
-                if(nums[m]==target && left == -1)
-                {
-                    left = m;
-                    right=R;
-                }
-                R =m;
-            }
-        }
-        if(nums[L]!=target)
-            return wrong;
-        lres =L;
-        L =left;
-        R=right;
-        
-        while(L<R)
-        {
-            m=floor((L+R)/2);
-            
-            if(nums[m] > target)
-            {
-                R=m;
-            }
-            else
-            {
-                L=m+1;
-            }
-            
-           
-            
-        }
-         rres = R-1;
-        return vector<int>({lres,rres});
+        auto low=lower_bound(begin(nums), end(nums), target);
+        auto up= upper_bound(begin(nums), end(nums), target);
+        if(low == end(nums) || *low!=target)
+            return {-1, -1};
+        return {(int)distance(begin(nums), low),
+                (int)distance(begin(nums), up-1)};
     }
 };
